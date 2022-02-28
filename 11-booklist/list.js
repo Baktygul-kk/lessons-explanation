bf7a39d7f0a7353aec351c57fe58e4ed5f24d41e
Baktygul Kozhoshova <baktygul.kozhoshova.kk@gmail.com>
2022-02-28 22:53:44 +0600

    11-booklist

---
+++ 11-booklist/list.js
@@ -0,0 +1,21 @@
+function Book () {
+    constructor(title, author, isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+function UI (){}
+
+UI.prototype.addBookList=function(book){
+
+};
+
+
+UI.prototype.clearFields=function(){};
+
+
+document.getElementById("form").addEventListener("submit". (e)=>{
+
+});
