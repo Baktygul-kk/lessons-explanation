4786cf95dbb16f717dc5408b11dc86d24810a8f4
Baktygul Kozhoshova <baktygul.kozhoshova.kk@gmail.com>
2022-03-01 23:32:54 +0600

    add

--- 11-booklist/list.js
+++ 11-booklist/list.js
@@ -6,15 +6,20 @@ function Book (title, author, isbn){
 
 function UI() { }
 
-UI.prototype.addBookList = function (book) {
-const table = document.getElementById("list");
-const tr = document.createElement("tr");
+// UI.prototype.addBookList = function (book) {
+// const table = document.getElementById("list");
+// const tr = document.createElement("tr");
+// console.log(table.append(tr));
 
-};
+// };
 
 
 UI.prototype.clearFields = function () {};
 
+
+Book.prototype.toString = function bookToString() {
+    return ` ${this.title} is a ${this.author} ${this.isbn}`;
+  }
 
 document.getElementById("book-form").addEventListener("submit", (e) => {
    e.preventDefault();
@@ -26,6 +31,13 @@ document.getElementById("book-form").addEventListener("submit", (e) => {
    
    const book = new Book (title, author, isbn);
    const ui = new UI ();
+   //ui.addBookList(book);
+
+   const table = document.getElementById("list");
+   const tr = document.createElement("tr");
+   console.log(table.append(tr));
+  const m = Object.values(book);
+   tr.innerHTML=m;
 
    console.log(book);
 
